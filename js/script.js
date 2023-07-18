e6539bedd8a7ff3295a9ab31a1fc740ca7bc6e55
Jaafar Mortada <jaafar1899@outlook.com>
2023-07-19 01:38:57 +0300

    remove button now subtracts from the total

--- js/script.js
+++ js/script.js
@@ -7,30 +7,35 @@ function expenseRow(name, price) {
             <td>
                 ${name}
             </td>
-            <td>
+            <td class="td-price">
                 ${price}
             </td>
         </tr>
     `
 }
 
+let total_expenses = 0
+
 function addExpenseRow() {
     const expense_name_input = $("#expense-name")
     const expense_price_input = $("#expense-price")
-    console.log(expense_name_input.val())
+    total_expenses += parseFloat(expense_price_input.val())
     if (expense_name_input.val().trim() === "" || expense_price_input.val().trim() === "" ) return
     const new_row = $(expenseRow(expense_name_input.val(), expense_price_input.val()))
     new_row.find(".remove-row").click(function () {
         new_row.remove()
+        const price_to_remove = new_row.find(".td-price")
+        total_expenses -= parseFloat(price_to_remove.text())
+        $('.total-sum').text(total_expenses)
     })
     $('#expenses-table tr#total_row').before(new_row)
     expense_name_input.val("")
     expense_price_input.val("")
+    $('.total-sum').text(total_expenses)
 }
 
 
 $(document).ready(function () {
     const add_row_btn = $("#add-row-btn")
     add_row_btn.click(addExpenseRow)
-    console.log("hii")
 })
