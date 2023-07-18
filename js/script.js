731a2ee2b73047d627eb5747661643409c55d64a
Jaafar Mortada <jaafar1899@outlook.com>
2023-07-19 01:19:55 +0300

    added functionality fo the add button

--- js/script.js
+++ js/script.js
@@ -13,3 +13,23 @@ function expenseRow(name, price) {
         </tr>
     `
 }
+
+function addExpenseRow() {
+    const expense_name_input = $("#expense-name")
+    const expense_price_input = $("#expense-price")
+    console.log(expense_name_input.val())
+    if (expense_name_input.val().trim() === "" || expense_price_input.val().trim() === "" ) return
+
+    $('#expenses-table tr#total_row').before(expenseRow(expense_name_input.val() ,
+        expense_price_input.val()))
+    
+    expense_name_input.val("")
+    expense_price_input.val("")
+}
+
+
+$(document).ready(function () {
+    const add_row_btn = $("#add-row-btn")
+    add_row_btn.click(addExpenseRow)
+    console.log("hii")
+})
